4eb03aa8a25c2aa452115f8a49a04caa4d8a38e2
Anupal97 <anupal@kursaha.com>
2023-02-11 12:22:20 +0530

    added comments in kursaha-test

--- src/test/java/com/kursaha/KursahaClientTest.java
+++ src/test/java/com/kursaha/KursahaClientTest.java
@@ -6,19 +6,23 @@ import org.junit.jupiter.api.Test;
 
 class KursahaClientTest {
 
-    private final String apiKey = System.getenv("key");
+    /**
+     * Create a unique api key from https://kursaha.com/engage-data-drive/api-key. After that, please add that api key
+     * into system environment.
+     */
+    private final String apiKey = System.getenv("key"); // Name of the Environment variable
     private KursahaClient kursahaClient;
 
     private String emitterId;
 
     @BeforeEach
     public void init() {
-        kursahaClient = new KursahaClient(apiKey, "http://localhost:8082/api/", "http://localhost:8081/api/");
-        emitterId = "123";
+        kursahaClient = new KursahaClient(apiKey);
+        emitterId = "<UNIQUE_EMITTER_ID>";
     }
 
     @Test
     public void testSendLoginEvent() throws Exception {
-        kursahaClient.edd.sendEventFlow(13L, "start_event", new JsonObject(), emitterId);
+        kursahaClient.edd.sendEventFlow(-1L /* WorkflowId */, "<Step_Node_Id>", new JsonObject() /* User Custom Payload*/, emitterId);
     }
 }
