c68e12d3d2e00bdaaa9e0724830571ead23e6e53
Anupal97 <anupal@kursaha.com>
2023-03-31 11:51:14 +0530

    fixed error message

--- src/main/java/com/kursaha/common/ErrorMessageDto.java
+++ src/main/java/com/kursaha/common/ErrorMessageDto.java
@@ -2,10 +2,13 @@ package com.kursaha.common;
 
 import com.google.gson.JsonArray;
 import lombok.Data;
+import lombok.ToString;
+
 /**
  * Response in case of error
  */
 @Data
+@ToString
 public class ErrorMessageDto {
     private String status;
     private String type;
