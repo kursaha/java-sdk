ea0652450bfbd1969eca6b59489d9923d4865b75
Anupal97 <anupal@kursaha.com>
2023-09-01 16:11:59 +0530

    fixed review comments

--- src/main/java/com/kursaha/common/ErrorMessageDto.java
+++ src/main/java/com/kursaha/common/ErrorMessageDto.java
@@ -7,11 +7,22 @@ import lombok.ToString;
  * Response in case of error
  */
 @Data
-@ToString
 public class ErrorMessageDto {
     private String status;
     private String type;
     private String title;
     private String message;
     private JsonArray fieldErrors;
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("ErrorMessageDto{");
+        sb.append("status='").append(status).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append(", title='").append(title).append('\'');
+        sb.append(", message='").append(message).append('\'');
+        sb.append(", fieldErrors=").append(fieldErrors);
+        sb.append('}');
+        return sb.toString();
+    }
 }
---
+++ src/main/java/com/kursaha/common/MailkeetsException.java
@@ -0,0 +1,2 @@
+package com.kursaha.common;public class MailkeetsException {
+}
